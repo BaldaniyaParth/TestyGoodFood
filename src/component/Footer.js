e2a4768a8e486bbb94084c12ca675d0615c746fc
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-03-13 11:17:49 +0530

    Footer CSS change

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,7 +1,8 @@
 const Footer = () => {
     return (
         <div className="footer">
-            <h2>Created By &hearts; Parth Baldaniya &copy; 2024 TestyGoodFood</h2>
+            <h2>Created By <span className="heart">&hearts;</span> Parth Baldaniya </h2>
+            <h2>&copy; 2024 <span className="logo-name">TestyGoodFood</span></h2>
         </div> 
     )
 }
