b1f373be706110da19a836d783120b86934e63e4
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-03-14 14:45:54 +0530

    Modify Body Component

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,22 +1,45 @@
 import { restaurantList } from "../Contant";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+
+function filterData(searchText, restaurants){
+    const filterData = restaurants.filter((restaurant) => 
+        restaurant.data.name.includes(searchText)
+    );
+    return filterData;
+}
 
 const Body = () => {
-    return(
-        <>
-            <div className="search">
-                <input type="text" className="search-input" placeholder="Enter the Food Name"/>
-                <button className="search-btn">Search</button>
-            </div>
-            <div className="restaurant-list">
-                {restaurantList.map((restaurant) => {
-                    return (
-                        <RestaurantCard key={restaurant.data.id} {...restaurant.data} />
-                    );
-                })}
-            </div>
-        </>
-    )
-}
+
+    const [restaurants, setRestaurants] = useState(restaurantList);
+    const [searchText, setSearchText] = useState("");
+
+  return (
+    <>
+      <div className="search">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Enter the Food Name"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button className="search-btn" 
+            onClick={() => {
+                const data = filterData(searchText, restaurants);
+                setRestaurants(data);
+            }}
+            >Search</button>
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.data.id} {...restaurant.data} />
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default Body;
