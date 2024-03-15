811b9d60556f4b0742e85cdf451b3ef1e839e987
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-03-15 11:28:28 +0530

    Shimmer Add

--- src/component/Body.js
+++ src/component/Body.js
@@ -17,26 +17,35 @@ const Body = () => {
     getRestaurant();
   }, []);
 
-
   const getRestaurant = async () => {
     try {
-      
-      const data = await fetch("https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
+      const data = await fetch(
+        "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
+      );
       const json = await data.json();
-      setAllRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-      setFilteredRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setAllRestaurants(
+        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+      setFilteredRestaurants(
+        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
     } catch (error) {
-      document.write(<h1>This is live swiggy api so fetching data not properly give response. I will work in this problem and solve very soon. Thank You</h1>)
+      document.write(
+        <h1>
+          This is live swiggy api so fetching data not properly give response. I
+          will work in this problem and solve very soon. Thank You
+        </h1>
+      );
     }
   };
 
-  if(!allRestaurants){
+  if (!allRestaurants) {
     return null;
   }
-  
-  return allRestaurants?.length == 0 ? (
-    <Shimmer />
-  ) : (
+
+  return (
     <>
       <div className="search">
         <input
@@ -46,26 +55,36 @@ const Body = () => {
           value={searchText}
           onChange={(e) => setSearchText(e.target.value)}
         />
-        <button className="search-btn" onClick={ () => {
-          const data = filterData(searchText, allRestaurants);
-          setFilteredRestaurants(data);
-        }}>
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchText, allRestaurants);
+            setFilteredRestaurants(data);
+          }}
+        >
           Search
         </button>
       </div>
-        <div className="restaurant-list">
-          {filteredRestaurants?.length == 0  ? (
-            <h1>Not Restaurant match your Filter!</h1>
-          ) : 
-          (filteredRestaurants?.map((restaurant) => (
+      <div className="restaurant-list">
+        {allRestaurants?.length === 0 ? (
+          <>
+            {[...Array(20)].map((_, index) => (
+              <Shimmer key={index} />
+            ))}
+          </>
+        ) : filteredRestaurants?.length == 0 ? (
+          <h1>Not Restaurant match your Filter!</h1>
+        ) : (
+          filteredRestaurants?.map((restaurant) => (
             <>
               <RestaurantCard
                 key={restaurant?.info?.id}
                 {...restaurant?.info}
               />
             </>
-          )))}
-        </div>
+          ))
+        )}
+      </div>
     </>
   );
 };
--- src/component/Shimmer.js
+++ src/component/Shimmer.js
@@ -1,7 +1,17 @@
-const Shimmer = () => {
-    return(
-        <h1 className="loading">Shimmer UI Loading.......</h1>
-    )
-}
 
-export default Shimmer
+const Shimmer = () => {
+  return (
+    <div className="card shimmer">
+      <div className="food-image shimmer"></div>
+      <div className="content">
+        <div className="food-name-shimmer shimmer"></div>
+        <div className="food-avg-shimmer shimmer"></div>
+        <div className="food-min-shimmer shimmer"></div>
+        <div className="food-cuisines-shimmer shimmer"></div>
+        <div className="food-area-shimmer shimmer"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Shimmer;
