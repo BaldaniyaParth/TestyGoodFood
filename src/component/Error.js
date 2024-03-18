87d91248215ae2b4297e690a1d2d9793484ef890
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-03-18 14:22:07 +0530

    Error Page Create

--- src/component/Error.js
+++ src/component/Error.js
@@ -1,7 +1,18 @@
+import ErrorImage from "../assets/img/404 Error.jpg";
+import { useRouteError, Link } from "react-router-dom"; // import useRouteError for routing error
+
 const Error = () => {
-    return (
-        <h1>Error</h1>
-    )
-}
+  const err = useRouteError();
+  return (
+    <div className="error-page">
+      <img src={ErrorImage} alt="Error Image" />
+      <h1>Oops! The restaurant you're looking for can't be found.</h1>
+      <h3 className="error-data">{err.data}</h3>
+      <h3 className="error-back-home">
+        <Link to="/">Back Home</Link>
+      </h3>
+    </div>
+  );
+};
 
 export default Error;
